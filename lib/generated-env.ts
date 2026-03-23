fed12fb139ffe276ce765239bf0de5f3e1995117
mbyd402 <mbyd402@users.noreply.github.com>
2026-03-23 13:02:24 +0800

    FIX: correct PayPal client ID

--- lib/generated-env.ts
+++ lib/generated-env.ts
@@ -4,7 +4,7 @@
 export const publicEnv = {
   "NEXT_PUBLIC_SUPABASE_URL": "https://mtjprdkzncxbvbdhodmt.supabase.co",
   "NEXT_PUBLIC_SUPABASE_ANON_KEY": "sb_publishable_A5P4p6bg0RYb1P8Ko9Bolw_hSM2Pg1-",
-  "NEXT_PUBLIC_PAYPAL_CLIENT_ID": "EMD60fBt-AmBvn_fd-7xCZ6TSi2jqxcgINubczPSu__eFUxhsr8LHq0EKF6GvqHJfrjR4uoWVeTf-Q6p"
+  "NEXT_PUBLIC_PAYPAL_CLIENT_ID": "AVDj9aDAeI7QkkxjPQ4r37YmftoJuvVJ2xbt36k2oGCVz9HQdXpBkNaeK6TiR-bBMFHE1N-e7ZHdPYpp"
 }
 
 export default publicEnv
